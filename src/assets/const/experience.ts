91f245c530d2d1b35bae894b753f878e0e167b73
carlos <caralfmo@gmail.com>
2025-07-23 22:39:25 -0500

    ajuste hero y estilos en componentes para ver en mobile

--- src/assets/const/experience.ts
+++ src/assets/const/experience.ts
@@ -3,7 +3,7 @@ import type { Experience } from '@/types/experience';
 export const EXPERIENCE: Experience[] = [
     {
         company: 'Ducor',
-        title: 'Desarrollador fullstack',
+        title: 'Desarrollador backend',
         startDate: '2023-08',
         endDate: '2024-10',
         description: 'Desarrollé aplicaciones internas y sistemas de gestión, implementando APIs, bases de datos y soluciones para integración entre plataformas. Me encargué del mantenimiento y actualización de sistemas existentes, automatización de procesos y soporte técnico a otras áreas.'
